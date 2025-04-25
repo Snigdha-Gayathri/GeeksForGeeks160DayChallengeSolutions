4c011ed3a970acf0088e717c16a1c2ee23c240a3
Snigdha Gayathri <156105138+Snigdha-Gayathri@users.noreply.github.com>
2025-04-25 21:40:38 +0530

    Add files via upload

---
+++ Day28.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    public:
+      int countFreq(vector<int>& arr, int target) {
+          // code here
+          int n=arr.size();
+          int count=0;
+          for(int i=0;i<n;i++)
+          {
+              if(arr[i]==target)
+              {
+                  count++;
+              }
+              
+          }
+          return count;
+      }
+  };
