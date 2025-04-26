64c6a26cae5740da93aba2fc4599494926fe3963
Snigdha Gayathri <156105138+Snigdha-Gayathri@users.noreply.github.com>
2025-04-26 20:23:58 +0530

    Add files via upload

---
+++ Day29.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    public:
+      int findMin(vector<int>& arr) {
+          // complete the function here
+          
+          int low=0;
+          int high=arr.size()-1;
+          while(low<high)
+          {
+              int mid=low+(high-low)/2;
+              if(arr[mid]>arr[high])
+              {
+                  low=mid+1;
+              }
+              else
+              {
+                  high=mid;
+              }
+          }
+          return arr[low];
+      }
+  };
