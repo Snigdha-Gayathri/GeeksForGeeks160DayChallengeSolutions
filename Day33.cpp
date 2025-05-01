a7d9f905db7a396b81c497d6c904d61fadba3705
Snigdha Gayathri <156105138+Snigdha-Gayathri@users.noreply.github.com>
2025-05-01 17:26:53 +0530

    Add files via upload

---
+++ Day33.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    public:
+      bool canPlaceCows(vector<int> & stalls, int k, int minDist)
+      {
+          int count=1;
+          int lastPos=stalls[0];
+          for(int i=1;i<stalls.size();i++)
+          {
+              if(stalls[i]-lastPos>=minDist)
+              {
+                  count++;
+                  lastPos=stalls[i];
+              }
+              if(count>=k) return true;
+          }
+          return false;
+      }
+  
+      int aggressiveCows(vector<int> &stalls, int k) {
+  
+          // Write your code here
+          sort(stalls.begin(),stalls.end());
+          int low=1;
+          int high=stalls.back()-stalls.front();
+          int result=0;
+          while(low<=high)
+          {
+              int mid=low+(high-low)/2;
+              if(canPlaceCows(stalls,k,mid))
+              {
+                  result=mid;
+                  low=mid+1;
+              }
+              else
+              {
+                  high=mid-1;
+              }
+          }
+          return result;
+          
+      }
+  };
