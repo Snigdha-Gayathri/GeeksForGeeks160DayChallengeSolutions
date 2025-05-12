9913def5a0c228904c342eee224bfa2c56803586
Snigdha Gayathri <156105138+Snigdha-Gayathri@users.noreply.github.com>
2025-05-12 18:06:25 +0530

    Add files via upload

---
+++ Day45.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    vector<int> intersectionWithDuplicates(vector<int>& a, vector<int>& b) {
+        // code here
+        unordered_set<int> setA(a.begin(),a.end());
+        unordered_set<int>result;
+        for(int num:b)
+        {
+            if(setA.find(num)!=setA.end())
+            {
+                result.insert(num);
+            }
+        }
+        return vector<int>(result.begin(),result.end());
+    }
+};
