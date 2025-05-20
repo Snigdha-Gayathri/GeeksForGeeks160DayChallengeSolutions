4da769bcbc409e0abdc389c794a5787e497f9bdb
Snigdha Gayathri <156105138+Snigdha-Gayathri@users.noreply.github.com>
2025-05-20 15:50:12 +0530

    Add files via upload

---
+++ Day52.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int countPairs(vector<int> &arr, int target) {
+        // Your code here
+        sort(arr.begin(),arr.end());
+        int left=0;
+        int right=arr.size()-1;
+        int count=0;
+        while(left<right)
+        {
+            if(arr[left]+arr[right]<target)
+            {
+                count+=(right-left);
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return count;
+        
+    }
+};
