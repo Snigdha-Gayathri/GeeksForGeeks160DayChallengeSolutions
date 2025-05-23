85a6a1dc2e98f8387dfcbf206f44a3523810aa30
Snigdha Gayathri <156105138+Snigdha-Gayathri@users.noreply.github.com>
2025-05-23 14:39:54 +0530

    Add files via upload

---
+++ Day55.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    // Function to count the number of possible triangles.
+    int countTriangles(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        int count=0;
+        sort(arr.begin(),arr.end());
+        for(int i=n-1;i>=2;i--)
+        {
+            int left=0;
+            int right=i-1;
+            while(left<right)
+            {
+                if(arr[left]+arr[right]>arr[i])
+                {
+                    count+=(right-left);
+                    right--;
+                }
+                else
+                {
+                    left++;
+                }
+            }
+        }
+        return count;
+        
+    }
+};
